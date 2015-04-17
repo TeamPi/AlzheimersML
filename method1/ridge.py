41269975ed759e35bb989eb977b836d619f760ac
Liam Dickson <liam@Liams-MacBook-Air.local>
2015-04-17 14:21:38 -0400

    ridge test using linear (not ridge)

--- method1/ridge.py
+++ method1/ridge.py
@@ -1,10 +1,26 @@
 #!/usr/local/bin/python
+import sys
+import csv
 from sklearn import linear_model
 
 def main():
-   clf = linear_model.Ridge (alpha = 0.5)
-   print clf.fit ([[0, 0], [0, 0], [1, 1]], [0, .1, 1])
-   print clf.coef_
-   print clf.intercept_
+    f = open(sys.argv[1], 'rt')
+    data_rows = csv.reader(f)
+
+    x_values = [];
+    y_values = [];
+    for i,row in enumerate(data_rows):
+        if i == 0:
+            continue
+        for j,cell in enumerate(row):
+            if j != 0:
+                row[j] = float(cell)
+        x_values.append(row[2:])
+        y_values.append(row[1])
+    clf = linear_model.LinearRegression()
+    clf.fit(x_values,y_values)
+    print clf.coef_
+
+    f.close()
 
 main()
