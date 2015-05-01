887d49901477c89a8d57820bdc1c6e9b3e49b730
jrb10014 <james.boivie@uconn.edu>
2015-05-01 14:16:30 -0400

    Added some graphing code

--- analysis.py
+++ analysis.py
@@ -1,6 +1,8 @@
 import csv
 import sys
 import math
+import matplotlib.pyplot as plt
+import pprint
 
 def analyze(run_data):
 	with open(run_data, 'r') as f:
@@ -8,20 +10,36 @@ def analyze(run_data):
 		lines = list(data)
 	length = float(0)
 	mse_sum = 0
+	diff_max = 0
+	diff_vec = []
 	for row in lines:
 		length += 1
-		mse_sum += math.pow(float(row[0])-float(row[1]), 2)
+		diff = float(row[1])-float(row[0])
+		diff_max = max(diff_max, abs(diff))
+		diff_vec.append(diff)
+		mse_sum += math.pow(diff, 2)
 	rmse = math.sqrt(mse_sum/length)
-	print 'Root Mean Squared Error: '+str(rmse)
-	#How many are within 1 standard deviation?
-	count = 0
-	for row in lines:
-		if abs(float(row[0])-float(row[1])) <= rmse:
-			count += 1
-	print 'Percentage of estimates within 1 standard deviation: '+str((count/length)*100)+'%'
-	#How many are within 2 standard deviations?
-	count = 0
-	for row in lines:
-		if abs(float(row[0])-float(row[1])) <= 2*rmse:
-			count += 1
-	print 'Percentage of estimates within 2 standard deviations: '+str((count/length)*100)+'%'
+
+	#Bin differences by frequency
+	#Bin size is half the RMSE
+	#How many
+	num_bins = int(math.ceil(diff_max/(rmse/2)))
+	bins = [x*(rmse/2) for x in range(-1*num_bins, num_bins)]
+	freq_vec = []
+	for i in xrange(0, len(bins)-1):
+		freq = 0
+		for e in diff_vec:
+			if bins[i] <= e < bins[i+1]:
+				freq += 1
+		freq_vec.append(freq)
+
+	#pprint.pprint(freq_vec)
+	#pprint.pprint(bins)
+
+	#Do plotting
+	plt.plot(bins[0:len(bins)-1], freq_vec)
+	plt.xticks(bins[0:len(bins)-1])
+	plt.show()
+
+
+analyze(sys.argv[1])
